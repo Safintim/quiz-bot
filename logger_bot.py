dbf8e7289f22eb4e6b583ee5f76beae67f51f46c
Safintim <Sarami96>
2019-06-06 11:57:52 +0300

    Refactoring - removed unnecessary func

--- logger_bot.py
+++ logger_bot.py
@@ -1,21 +1,6 @@
 import os
 import logging
 import telegram
-from functools import partial
-
-
-def create_bot_logger(token):
-    bot = telegram.Bot(token)
-    return bot
-
-
-def send_report(bot, chat_id, msg):
-    bot.send_message(chat_id=chat_id, text=msg)
-
-
-send_report = partial(send_report,
-                      create_bot_logger(os.getenv('LOGGER_BOT_TOKEN')),
-                      os.getenv('LOGS_RECEIVER_ID'))
 
 
 def create_logger(logs_handler):
@@ -31,10 +16,11 @@ class LogsHandler(logging.Handler):
 
     def __init__(self):
         super().__init__()
+        self.bot = telegram.Bot(os.getenv('LOGGER_BOT_TOKEN'))
 
     def emit(self, record):
         log_entry = self.format(record)
-        send_report(log_entry)
+        self.bot.send_message(chat_id=os.getenv('LOGS_RECEIVER_ID'), text=log_entry)
 
 
 logger = create_logger(LogsHandler())
