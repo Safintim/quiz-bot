eba27ee45bfe8d4032358597c2d0558c535be17a
Safintim <Sarami96>
2019-06-06 12:38:02 +0300

    Refactoring - rename module

--- log_conf.py
+++ log_conf.py
@@ -3,15 +3,6 @@ import logging
 import telegram
 
 
-def create_logger(logs_handler):
-    logger = logging.getLogger('Bot Logger')
-    handler = logs_handler
-    handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
-    logger.addHandler(handler)
-    logger.setLevel(logging.INFO)
-    return logger
-
-
 class LogsHandler(logging.Handler):
 
     def __init__(self):
@@ -22,5 +13,3 @@ class LogsHandler(logging.Handler):
         log_entry = self.format(record)
         self.bot.send_message(chat_id=os.getenv('LOGS_RECEIVER_ID'), text=log_entry)
 
-
-logger = create_logger(LogsHandler())
